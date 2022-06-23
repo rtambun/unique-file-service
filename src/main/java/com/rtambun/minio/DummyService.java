427a108324a6c99c4f11e07a4409c1b90327cb3e
rtambun <richson.tambun@thalesgroup.com>
2022-06-23 16:32:36 +0800

    Minio java sdk is updated to 8.4.1 and NotificationInfo is replaced with NotificationRecords.

--- src/main/java/com/rtambun/minio/DummyService.java
+++ src/main/java/com/rtambun/minio/DummyService.java
@@ -4,13 +4,12 @@ package com.rtambun.minio;
 import com.jlefebure.spring.boot.minio.MinioException;
 import com.jlefebure.spring.boot.minio.MinioService;
 import com.jlefebure.spring.boot.minio.notification.MinioNotification;
-import io.minio.notification.NotificationInfo;
+import io.minio.messages.NotificationRecords;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -22,20 +21,20 @@ public class DummyService {
     private MinioService minioService;
 
     @MinioNotification({"s3:ObjectCreated:Put"})
-    public void handleUpload(NotificationInfo notificationInfo) {
-        log.info(Arrays
-            .stream(notificationInfo.records)
-            .map(notificationEvent -> "Receiving event " + notificationEvent.eventName + " for " + notificationEvent.s3.object.key)
+    public void handleUpload(NotificationRecords notificationRecords) {
+        log.info(notificationRecords.events()
+            .stream()
+            .map(notificationEvent -> "Receiving event " + notificationEvent.eventType().name() + " for " + notificationEvent.objectName())
             .collect(Collectors.joining(","))
         );
     }
 
     @MinioNotification(value = {"s3:ObjectAccessed:Get"}, suffix = ".pdf")
-    public void handleGetPdf(NotificationInfo notificationInfo) {
-        Arrays.stream(notificationInfo.records)
+    public void handleGetPdf(NotificationRecords notificationRecords) {
+        notificationRecords.events().stream()
                 .map(notificationEvent -> {
                     try {
-                        return minioService.getMetadata(Path.of(notificationEvent.s3.object.key));
+                        return minioService.getMetadata(Path.of(notificationEvent.objectName()));
                     } catch (MinioException e) {
                         log.info(e.toString());
                         return null;
@@ -43,7 +42,7 @@ public class DummyService {
 
                 })
                 .filter(Objects::nonNull)
-                .forEach(objectStat -> log.info("metadata for " + objectStat.name() + objectStat.httpHeaders()));
+                .forEach(objectStat -> log.info("metadata for " + objectStat.object() + "." + objectStat.contentType()));
     }
 
     public String helloWorld()
