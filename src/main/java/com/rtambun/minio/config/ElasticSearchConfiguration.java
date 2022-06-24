f088cc93ed14e8ec0d17d1c044345c288a5a6490
rtambun <richson.tambun@thalesgroup.com>
2022-06-24 13:26:45 +0800

    Update ElasticSearchConfiguration to use ElasticsearchClient.

--- src/main/java/com/rtambun/minio/config/ElasticSearchConfiguration.java
+++ src/main/java/com/rtambun/minio/config/ElasticSearchConfiguration.java
@@ -1,50 +1,43 @@
 package com.rtambun.minio.config;
 
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import javax.annotation.Nonnull;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.rtambun.minio.repository")
-public class ElasticSearchConfiguration {
+public class ElasticSearchConfiguration extends ElasticsearchConfiguration {
 
-    @Value("${elasticsearch.host}")
-    private String elasticSearchHost;
+    private final String elasticSearchHost;
+    private final int elasticSearchPort;
+    private final String elasticSearchUserName;
+    private final String elasticSearchPassword;
 
-    @Value("${elasticsearch.port}")
-    private int elasticSearchPort;
+    public ElasticSearchConfiguration(@Value("${elasticsearch.host}") String elasticSearchHost,
+                                      @Value("${elasticsearch.port}") int elasticSearchPort,
+                                      @Value("${elasticsearch.username}") String elasticSearchUserName,
+                                      @Value("${elasticsearch.password}") String elasticSearchPassword) {
+        this.elasticSearchHost = elasticSearchHost;
+        this.elasticSearchPort = elasticSearchPort;
+        this.elasticSearchUserName = elasticSearchUserName;
+        this.elasticSearchPassword = elasticSearchPassword;
+    }
 
-    @Value("${elasticsearch.username}")
-    private String elasticSearchUserName;
-
-    @Value("${elasticsearch.password}")
-    private String elasticSearchPassword;
-
-    @Bean
-    public RestHighLevelClient client() {
+    @Override
+    @Nonnull
+    public ClientConfiguration clientConfiguration() {
         if (elasticSearchUserName != null && !elasticSearchUserName.isBlank()
                 && elasticSearchPassword != null && !elasticSearchPassword.isBlank()) {
-            ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+            return ClientConfiguration.builder()
                     .connectedTo(String.format("%s:%s", elasticSearchHost, elasticSearchPort))
                     .withBasicAuth(elasticSearchUserName, elasticSearchPassword)
                     .build();
-            return RestClients.create(clientConfiguration).rest();
         } else {
-            return(new RestHighLevelClient(
-                    RestClient.builder(new HttpHost(elasticSearchHost, elasticSearchPort, "http"))));
+            return ClientConfiguration.create(String.format("http://%s:%s", elasticSearchHost, elasticSearchPort));
         }
-    }
-
-    @Bean
-    public ElasticsearchOperations elasticsearchTemplate() {
-        return new ElasticsearchRestTemplate(client());
     }
 }
